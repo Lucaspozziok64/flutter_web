379442b55e0df8e70c587ad17c085f5e8964e360
EstebanQuito05 <est3b4n2018@gmail.com>
2024-10-19 02:49:07 -0300

    web

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Youtube'),
+        backgroundColor: Colors.red,
+        actions: const [
+          Icon(Icons.search),
+          Icon(Icons.camera_front),
+          Icon(Icons.person),
+        ]
+      ),
+    );
+  }
+}
